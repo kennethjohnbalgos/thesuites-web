6c838a9bf405578cf8e7f852dbe093d2eea67a39
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2012-04-17 17:09:24 +0800

    For Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThesuitesWeb::Application.config.secret_token = '9865890725dfda75689ae8a4a4a9492706b767614e49523093e2efdcd83e9c86d5dddd5302b249fdc1438a09948ccc62566620d0f92a892e1426cf7690e41f3e'
