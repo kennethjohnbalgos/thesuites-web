5a1a178943951a161b31bca1a059122fcc80662b
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2012-04-17 17:38:31 +0800

    Precompile application.css

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ end
 
 module ThesuitesWeb
   class Application < Rails::Application
+    config.assets.precompile += ['application.css', 'application.js']
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
